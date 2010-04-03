c665b4d4e4df9b8c8d550e558281802736261a2c
Pavol Rusnak <stick@gk2.sk>
2010-04-03 00:03:56 +0000

    add some info add reload button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var setFrameUrl = function(url) {
-  if (!url) return;
+  if (!url || url == 'undefined') return;
   if (!url.match('^https?://')) {
     url = 'http://' + url;
   }
@@ -7,12 +7,19 @@ var setFrameUrl = function(url) {
   $('iframe').attr('src',url);
 };
 
+var rotate = function() {
+  $('#ipad').toggleClass('landscape').toggleClass('portrait');
+}
+
 $(function(){
 
 setFrameUrl($.url.param('url'));
+if ($.url.param('portrait')) rotate();
 
-$('#rotate').click(function(){
-  $('#ipad').toggleClass('landscape').toggleClass('portrait');
+$('#rotate').click(rotate);
+
+$('#reload').click(function(){
+  $('iframe').attr('src',$('iframe').attr('src'));
 });
 
 $('#url').focus(function(){
@@ -23,10 +30,15 @@ $('#url').blur(function(){
   $('#kbd').hide();
 });
 
-$('#url').keyup(function(event){
-  if (event.keyCode != 13) return;
+$('#url').keyup(function(evt){
+  if (evt.keyCode != 13) return;
   $('#url').blur();
   setFrameUrl($(this).val());
+});
+
+$('#show_about').click(function(){
+  $('#about').slideToggle();
+  return false;
 });
 
 });
